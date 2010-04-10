55897a36d7425d3dde6f27bfeebb80c62571902c
Tom Crinson <the.jaba@gmail.com>
2010-04-10 18:56:44 +0100

    refactored drawing logic

--- public/javascripts/explosion.js
+++ public/javascripts/explosion.js
@@ -6,57 +6,49 @@ function Explosion(x, y){
 	this.frameHeight = 93;
 	this.frameCount = 5;
 	this.direction = 0;
+	this.maxRadius = 3;
 };
 
 Explosion.prototype = {
+	
 	draw:function(){
-		var explosion = this;
-		$.each([0,1,2,3], function(direction){
-			for( var frame = 0; frame < 2; frame ++ ){
-				var drawX = explosion.drawXPosition( direction, frame ) + explosion.x; 
-				var drawY = explosion.drawYPosition( direction, frame ) + explosion.y;
-				var drawFrame = parseInt(Math.random()*2);
-				if( explosion.canDrawInTile(drawX, drawY) && drawX >= -20 && drawX < explosion.canvas.width && drawY >= -40 && drawY < explosion.canvas.height ){
-					explosion.canvas.getContext('2d').drawImage(explosion.img, drawFrame * explosion.frameWidth, direction * explosion.frameHeight, explosion.frameWidth, explosion.frameHeight, drawX + 10, drawY +40, explosion.frameWidth, explosion.frameHeight);
-				}else{
-					break;
-				}
-			}			
-		})		
+		var self = this;
+		this.tilesAround( this.getTileX(), this.getTileY(), function(tileX, tileY, direction, radius){
+			var drawFrame = parseInt(Math.random()*2);
+			var drawX = tileX * self.frameWidth;
+			var drawY = tileY * self.frameHeight;
+			if( drawX >= -20 && drawX < self.canvas.width && drawY >= -40 && drawY < self.canvas.height ){
+				self.canvas.getContext('2d').drawImage(self.img, drawFrame * self.frameWidth, direction * self.frameHeight, self.frameWidth, self.frameHeight, drawX + 10, drawY +40, self.frameWidth, self.frameHeight);
+			}
+		});
 	},
 	
-	drawXPosition:function(direction, frame){
-		if( direction === 1 ){
-			return frame * MrJaba.Bomberman.Images.tileWidth() * -1;
-		}else if( direction === 2 )
-		{
-			return frame * MrJaba.Bomberman.Images.tileWidth() * 1;
-		}
-		return 0;
+	tilesAround:function(tileX, tileY, action){
+		for( var direction = 0; direction < 4; direction++ ){
+			for( var radius = 0; radius < this.maxRadius; radius++ ){
+				var currentX = (direction === 1) ? tileX - radius : (direction === 2) ? tileX + radius : tileX;
+				var currentY = (direction === 0) ? tileY + radius : (direction === 3) ? tileY - radius : tileY;
+				if( this.containsObstruction( currentX, currentY ) ){ break; }//Ignore this direction as there is an obstruction
+				action(currentX, currentY, direction, radius);
+			}
+		}		
 	},
 	
-	drawYPosition:function(direction, frame){
-		if( direction === 0 ){
-			return frame * MrJaba.Bomberman.Images.visibleTileHeight() * 1;
-		}else if( direction === 3 )
-		{
-			return frame * MrJaba.Bomberman.Images.visibleTileHeight() * -1;
-		}
-		return 0;
-	},
-	
-	canDrawInTile:function(drawX, drawY){
-		var tileX = this.getTileX(drawX);
-		var tileY = this.getTileY(drawY);
-		return ( tileX >= 0 && tileY >= 0 && tileX < MrJaba.Bomberman.map.length && tileY < MrJaba.Bomberman.map[tileX].length && MrJaba.Bomberman.map[tileX][tileY].walkable);
+	containsObstruction: function(tileX, tileY){
+		return !( tileX >= 0 && tileY >= 0 && tileX < MrJaba.Bomberman.map.length && tileY < MrJaba.Bomberman.map[tileX].length && MrJaba.Bomberman.map[tileX][tileY].walkable)
 	},
 	
 	tick:function(){
+		this.checkForKills();
 		if( this.frame < this.frameCount ){
 			this.frame += 1;
 		}
 		else{
 			MrJaba.Bomberman.removeExplosion(this);
 		}
+	},
+	
+	checkForKills:function(){
+		
 	}
 }
